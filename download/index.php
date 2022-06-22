a4a7895bf3c732056b90e5e22dc6a7596948acd5
suryo <suryoatm@gmail.com>
2022-06-22 15:07:22 +0700

    add folder download

---
+++ download/index.php
@@ -0,0 +1,5 @@
+<?php
+header("Location: http://supresso.com/broc01/Brochure_Supresso_100220.pdf");
+die();
+
+?>
---
+++ download/indexbaru.php
@@ -0,0 +1,5 @@
+<?php
+header("Location: https://sidar.id/download/");
+die();
+
+?>
