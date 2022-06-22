98efcc5b4701062e8bb762f315683b3a87993056
suryo <suryoatm@gmail.com>
2022-06-22 15:03:20 +0700

    add folder app

---
+++ app/index.php
@@ -0,0 +1,5 @@
+<?php
+header("Location: https://sidar.id/app/Sidar v1.1.6a-SDK 29.apk");
+die();
+
+?>
---
+++ app/indexx.php
@@ -0,0 +1,5 @@
+<?php
+header("Location: https://sidar.id/app/Sidar v10106-1.apk");
+die();
+
+?>
